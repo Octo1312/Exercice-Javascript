3fb674081b0dead6054b7756d62f3cc02c6edd2a
Léo Martin <factorypacted@gmail.com>
2025-11-13 09:58:58 +0100

    Exo 11 + Pyramide

---
+++ exo_11.js
@@ -0,0 +1,7 @@
+for (let i = 1; i <= 9 ; i++) {
+    let X = "";
+    for (let u = 1; u <= 9 ; u++){
+        X += " " + u*i 
+    }
+    console.log(X)
+}
---
+++ exo_12.js
@@ -0,0 +1,7 @@
+let size = 10
+let temp = "⬛️"
+let space = " "
+for (let i = 1; i <= size ;i++) {
+    temp =  temp + space  + "⬛️"
+    console.log(temp)
+}
