41e9a5d4fdb27c748ac4c353a9ac5a7b7b43c0de
Léo Martin <factorypacted@gmail.com>
2025-12-09 15:16:52 +0100

    [NEW] Fin de l'exercice 4

---
+++ Ludo-Exo/Exercice4/script.js
@@ -0,0 +1,31 @@
+const result = document.querySelector('.result');
+const input = document.querySelectorAll('input');
+
+input[0].addEventListener("change", () => {
+    let r = input[0].value;
+    let g = input[0].value;
+    let b = input[0].value;
+    result.style.background = `rgb(${r}, ${g}, ${b})`;
+})
+
+input[1].addEventListener("change", () => {
+    let padding = input[1].value;
+    result.style.padding = `${padding}px`;
+})
+
+input[2].addEventListener("change", () => {
+    let height = input[2].value;
+    result.style.height = `${height}px`;
+})
+input[3].addEventListener("change", () => {
+    let width = input[3].value;
+    result.style.width = `${width}px`;
+})
+input[4].addEventListener("change", () => {
+    let cornered = input[4].value;
+    result.style.borderRadius = `${cornered}px`;
+})
+input[5].addEventListener("change", () => {
+    let rotation = input[5].value;
+    result.style.transform = `rotate(${rotation}deg)`;
+})
